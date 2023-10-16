25e99dc332a2622af5de965892a301ca040adb19
Yuri <119751708+ytksh@users.noreply.github.com>
2023-10-16 18:15:20 -0300

    adds crt monitor effects & simple lighting

--- scripts/player/PlayaController.cs
+++ scripts/player/PlayaController.cs
@@ -5,30 +5,20 @@ public class PlayaController : KinematicBody2D
 {
     [Export] public int speed = 200;
 
-    public Vector2 velocity = new Vector2();
 
-    public void GetInput()
-    {
-        velocity = new Vector2();
-
-        if (Input.IsActionPressed("ui_right"))
-            velocity.x += 1;
-
-        if (Input.IsActionPressed("ui_left"))
-            velocity.x -= 1;
-
-        if (Input.IsActionPressed("ui_down"))
-            velocity.y += 1;
-
-        if (Input.IsActionPressed("ui_up"))
-            velocity.y -= 1;
-
-        velocity = velocity.Normalized() * speed;
-    }
 
     public override void _PhysicsProcess(float delta)
     {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
+        if (Input.IsActionPressed("ui_right"))
+            MoveAndSlide(new Vector2(speed, 0));
+
+        if (Input.IsActionPressed("ui_left"))
+            MoveAndSlide(new Vector2(-speed, 0));
+
+        if (Input.IsActionPressed("ui_down"))
+            MoveAndSlide(new Vector2(0, speed));
+
+        if (Input.IsActionPressed("ui_up"))
+            MoveAndSlide(new Vector2(0, -speed));
     }
 }
