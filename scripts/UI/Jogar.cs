202f49100a02c58b30d4d21a488674e3aa7a918c
Caveirinha <70849034+LolILoveVisual@users.noreply.github.com>
2023-10-17 11:56:10 -0300

    Menu + Play Button

--- scripts/UI/Jogar.cs
+++ scripts/UI/Jogar.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Network : Node2D
+public class Jogar : Button
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
@@ -10,9 +10,11 @@ public class Network : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		this.Connect("pressed",this,"_OnPressed"); //vai conectar no "signal" do botão e vai chamar a função de pressionado
 	}
-
+	void _OnPressed() {
+			GetTree().ChangeScene("res://scenes/World.tscn");
+	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
