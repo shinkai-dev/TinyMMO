6f646d9617264e12f898681ea5174437c7f8fdd6
Caveirinha <70849034+LolILoveVisual@users.noreply.github.com>
2023-10-19 20:09:03 -0300

    NPC Bat + EnemyAI

--- scripts/player/PlayaController.cs
+++ scripts/player/PlayaController.cs
@@ -3,32 +3,30 @@ using System;
 
 public class PlayaController : KinematicBody2D
 {
-    [Export] public int speed = 100;
+	[Export] public int speed = 100;
+	public Vector2 velocity = new Vector2();
+	public void GetInput()
+	{
+		velocity = new Vector2();
 
-    public Vector2 velocity = new Vector2();
+		if (Input.IsActionPressed("ui_right"))
+			velocity.x += 1;
 
-    public void GetInput()
-    {
-        velocity = new Vector2();
+		if (Input.IsActionPressed("ui_left"))
+			velocity.x -= 1;
 
-        if (Input.IsActionPressed("ui_right"))
-            velocity.x += 1;
+		if (Input.IsActionPressed("ui_down"))
+			velocity.y += 1;
 
-        if (Input.IsActionPressed("ui_left"))
-            velocity.x -= 1;
+		if (Input.IsActionPressed("ui_up"))
+			velocity.y -= 1;
 
-        if (Input.IsActionPressed("ui_down"))
-            velocity.y += 1;
+		velocity = velocity.Normalized() * speed;
+	}
 
-        if (Input.IsActionPressed("ui_up"))
-            velocity.y -= 1;
-
-        velocity = velocity.Normalized() * speed;
-    }
-
-    public override void _PhysicsProcess(float delta)
-    {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		GetInput();
+		velocity = MoveAndSlide(velocity);
+	}
 }
