0e9a8291b2c01749a8a931f68017217e88e4dda7
Yuri <119751708+ytksh@users.noreply.github.com>
2023-10-19 21:32:19 -0300

    adds sprinting & footsteps
    Sound effects sampled from Atari 2600's Swashbuckler.

--- scripts/player/PlayaController.cs
+++ scripts/player/PlayaController.cs
@@ -9,10 +9,16 @@ public class PlayaController : KinematicBody2D
     [Export] public float sprintMultiplier = 1.6f;
     public Sprite sprite;
     public Vector2 velocity = new Vector2();
+    public Timer stepTimer;
+    public float ogStepTimer;
+    public AudioStreamPlayer2D stepSound;
 
     public override void _Ready()
     {
         sprite = GetNode<Sprite>("Playa");
+        stepSound = GetNode<AudioStreamPlayer2D>("stepSound");
+        stepTimer = GetNode<Timer>("stepTimer");
+        ogStepTimer = stepTimer.WaitTime;
     }
     
     public void GetInput(float delta)
@@ -38,16 +44,30 @@ public class PlayaController : KinematicBody2D
             sprite.FlipH = velocity.x < 0;
             break;
         }
-        velocity = velocity * MoveSpeed;
         if(Input.IsActionPressed("sprint")){
             velocity *= sprintMultiplier;
         }
+        if(stepTimer.TimeLeft == 0){
+            stepSound.Play();
+            stepSound.PitchScale = (float)GD.RandRange(0.8f, 1.2f);
+            if(Input.IsActionPressed("sprint")){
+                stepTimer.Start(ogStepTimer / sprintMultiplier);}
+            else{
+                stepTimer.Start(ogStepTimer);
+            }
+        }
+        velocity = velocity * MoveSpeed;
+
         
     }
 
     public override void _PhysicsProcess(float delta){
         GetInput(delta);
         velocity = MoveAndSlide (velocity);
+    }
+
+    public void _on_stepDelay_timeout(){
+        
     }
     
 }
