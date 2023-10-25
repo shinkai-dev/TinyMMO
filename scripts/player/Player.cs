f807246fe6381c4b7cf45de72cdc67dc79e4a05b
Yuri <119751708+ytksh@users.noreply.github.com>
2023-10-25 10:00:57 -0300

    Random player color & sprinting

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -5,17 +5,27 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	[Export] public int MoveSpeed = 16;
+	[Export] public int MoveSpeed = 80;
+	[Export] public float sprintMultiplier = 1.6f;
 	public Sprite sprite;
+	public Sprite playaEyes;
 	[Puppet] public Vector2 PuppetVelocity = new Vector2();
 	[Puppet] public Vector2 PuppetPosition = new Vector2();
 	public Vector2 Velocity = new Vector2();
+	public Godot.Color PlayaColor = new Color(0, 0, 0);
+	public Godot.Color PlayaEyeColor = new Color(0, 0, 0);
 
 	public override void _Ready()
 	{
 		sprite = GetNode<Sprite>("Playa");
+		playaEyes = GetNode<Sprite>("Playa/Playa-eyes");
 		PuppetPosition = Position;
 		PuppetVelocity = Velocity;
+		//temporario para poder diferenciar os jogadores
+		sprite.Modulate = PlayaColor;
+		sprite.SelfModulate = PlayaColor;
+		playaEyes.Modulate = PlayaEyeColor;
+		playaEyes.SelfModulate = PlayaEyeColor;
 	}
 	
 	public void GetInput(float delta)
@@ -33,8 +43,10 @@ public class Player : KinematicBody2D
 
 		if (Input.IsActionPressed("ui_up"))
 			Velocity.y -= 1;
-
-		Velocity *= delta * MoveSpeed;
+        if(Input.IsActionPressed("sprint")){
+            Velocity *= sprintMultiplier;
+        }
+		Velocity *= MoveSpeed;
 		Rset(nameof(PuppetVelocity), Velocity);
 		Rset(nameof(PuppetPosition), Position);
 	}
@@ -50,8 +62,9 @@ public class Player : KinematicBody2D
 		}
 
 		sprite.FlipH = Velocity.x < 0;
+		playaEyes.FlipH = Velocity.x < 0;
 
-		MoveAndCollide(Velocity);
+		MoveAndSlide(Velocity);
 	}
 }
 
