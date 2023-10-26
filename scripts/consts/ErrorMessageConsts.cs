d19e0cd8632bb770ca87cc872850430f91b3c0ac
Carlos Eduardo Prezzi <carlos.prezzi@be220.com>
2023-10-26 19:43:36 -0300

    fix: added missing error message

--- scripts/consts/ErrorMessageConsts.cs
+++ scripts/consts/ErrorMessageConsts.cs
@@ -10,6 +10,7 @@ public abstract class ErrorMessageConsts
         {"EMAIL_EXISTS", "The email address is already in use by another account."},
         {"OPERATION_NOT_ALLOWED", "Password sign-in is disabled for this project."},
         {"INVALID_LOGIN_CREDENTIALS", "The email address or password is incorrect."},
+        {"MISSING_PASSWORD", "Please, type a password"},
         {"TOO_MANY_ATTEMPTS_TRY_LATER", "We have blocked all requests from this device due to unusual activity. Try again later."}
     };
 }
