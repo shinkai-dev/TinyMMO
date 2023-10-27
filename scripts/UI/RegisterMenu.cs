8ecaf06dbdbd96b1826be57d83f920e57cad6850
Yuri <119751708+ytksh@users.noreply.github.com>
2023-10-26 21:25:05 -0300

    Improvements to theme & Random quote generator

--- scripts/UI/RegisterMenu.cs
+++ scripts/UI/RegisterMenu.cs
@@ -6,6 +6,7 @@ public class RegisterMenu : Node
 	public override void _Ready()
 	{
 		GetNode<Button>("Register").Connect("pressed", this, nameof(OnRegisterPressed));
+		GetNode<Button>("Return").Connect("pressed", this, nameof(onReturnPressed));
 	}
 
 	void OnRegisterPressed() {
@@ -13,5 +14,8 @@ public class RegisterMenu : Node
 			GetNode<LineEdit>("Email").Text,
 			GetNode<LineEdit>("Password").Text
 		);
+	}
+	void onReturnPressed() {
+		GetTree().ChangeScene("res://scenes/menu/menuPrin.tscn");
 	}
 }
